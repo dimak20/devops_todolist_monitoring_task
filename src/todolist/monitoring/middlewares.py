ecdec1a8f3172fb872a8202a1912dfff77d577e3
Dmytro Kolchak <dima.kolhac@gmail.com>
2026-01-07 04:28:04 +0200

    Implement solution

--- src/todolist/monitoring/middlewares.py
+++ src/todolist/monitoring/middlewares.py
@@ -1,28 +1,34 @@
 import time
-from prometheus_client import Counter, Summary
+from prometheus_client import Counter, Histogram  # Рекомендую Histogram для задержки
 
 REQUEST_COUNT = Counter(
     "django_http_requests_total",
-    "Total HTTP Requests",
+    "Total number of HTTP requests",
     ["method", "endpoint"]
 )
 
-REQUEST_LATENCY = Summary(
+REQUEST_LATENCY = Histogram(
     "django_http_requests_latency_seconds",
-    "HTTP Requests Creation Time"
+    "HTTP Request latency in seconds",
+    ["method", "endpoint"]
 )
+
 
 class PrometheusMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        REQUEST_COUNT.labels(method=request.method, endpoint=request.path).inc()
+        if request.path == '/metrics':
+            return self.get_response(request)
 
         start_time = time.time()
+
         response = self.get_response(request)
-        
+
+        REQUEST_COUNT.labels(method=request.method, endpoint=request.path).inc()
+
         resp_time = time.time() - start_time
-        REQUEST_LATENCY.observe(resp_time)
-        
+        REQUEST_LATENCY.labels(method=request.method, endpoint=request.path).observe(resp_time)
+
         return response
